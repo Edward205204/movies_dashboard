21c51ca9c01dfc15eda579f25b1cf4708e365ba0
Edward205204 <khoa123kd56cn@gmail.com>
2025-05-15 08:01:35 +0700

    chore: fix access token req

--- src/utils/http.axios.ts
+++ src/utils/http.axios.ts
@@ -10,15 +10,15 @@ class Http {
     this.accessToken = getAccessTokenFromLS();
     this.instance = axios.create({
       baseURL: config.baseURL,
-      timeout: 10000,
-      headers: {
-        'Content-Type': 'application/json'
-      }
+      timeout: 10000
+      // headers: {
+      //   'Content-Type': 'application/json'
+      // }
     });
 
     this.instance.interceptors.request.use((config) => {
       if (this.accessToken && config.headers) {
-        config.headers.Authorization = this.accessToken;
+        config.headers.Authorization = `Bearer ${this.accessToken}`;
         return config;
       }
       return config;
